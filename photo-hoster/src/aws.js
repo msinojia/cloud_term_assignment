69c8529121b5cd1c34de3324ffd83d267579a0c7
Meet Sinojia <meetsinojia7@gmail.com>
2023-07-31 15:32:24 -0300

    Remove chown line

--- photo-hoster/src/aws.js
+++ photo-hoster/src/aws.js
@@ -4,10 +4,10 @@ const s3BucketName = "photo-hoster-images";
 
 AWS.config.update({
   region: "us-east-1",
-  accessKeyId: "ASIAQWSBQAI3NI4ARWET",
-  secretAccessKey: "W9x1Qw9kE8/5aln6kkbwrAABuSLLxAF90GO/gcVz",
+  accessKeyId: "ASIAQWSBQAI3M6GH7BRX",
+  secretAccessKey: "nZfXJb7nrohWFUij1x1NNhGQNnklVYRPPZ0U+G7i",
   sessionToken:
-    "FwoGZXIvYXdzEAwaDKxrJD5o9NT1Movv9CLAAaACGTPkG8ZiPp6gNYPYkAvRNmiac2VomLeH29+0V1XKX4ShQBPV9kSIMi9NYqEXO/OexfIPDQwS4QNGsfrTOXgQvmsySUKUAtnTwNZVM7AsYR8Uv8XcwBkjp9KywR7mQbJ7zhWlEpjz7K9UCWQnrl9CXIbQL4oVZ+yJhk7qW9dCas2dkp8imz6UcheHcrAXqX1VbKkkgT7I7gqsTYNzYDE1h6d4HdLaRuj7J3wkTh80pVuR81+VuQxZ0Ryxxp12xij9r5WmBjItNCuTQRj+0ffRoxAqZZknUYF0y6oSOtjsrC/Ljg2rXBQFr1ikODYF5BeR04Bg",
+    "FwoGZXIvYXdzEDwaDFtOLmqsPrL0pWSlmSLAARXBkjaHBgxqyTAkCtAvg2WT5H6BmgzfZY6FWE0CkWpii1IFFUw17OokeEt66BGd26sLBJB8kG8JTfEFamtRCLpfwKwhkSEjVE+KNReQsUinlJbLqY99RIeSOlmUB9Y5vgqPdrbqKrTpXEiy6VBXSVym62Q63y8qpltUHu8vyT/63Kg9ehOUwoWFhJu3M869Rz4jDD/+C2eBL70/YSj0z1b0b6cP7QLdB0FLvMtG2icsVwUQFBSDIFPa4tduiCkMryjX75+mBjItJqGywcu8L1Mp0yCOUPyKSqWXxgze/5Fe0qfW5NEn3AvaeVQqAtVMqn3w3NFr",
 });
 
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
